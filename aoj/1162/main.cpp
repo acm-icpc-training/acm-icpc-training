08c58d46ff2e3ed0ca1646c92fea697f12588f58
ACM ICPC Training <acm.icpc.training@gmail.com>
2015-03-19 23:36:12 +0900

    Update AOJ 1162.

--- aoj/1162/main.cpp
+++ aoj/1162/main.cpp
@@ -17,12 +17,6 @@ struct Edge {
 struct VertexCost {
   int city, prev, speed;
   double cost;
-  int VertexID() {
-    int id = city;
-    id = MAX_NUM_CITY * id + prev;
-    id = (MAX_SPEED + 1) * id + speed;
-    return id;
-  }
 };
 
 bool GreaterCost(const VertexCost& vc1, const VertexCost& vc2) {
@@ -32,26 +26,16 @@ bool GreaterCost(const VertexCost& vc1, const VertexCost& vc2) {
 void Solve(int n, vector<vector<Edge>>& graph, int start, int goal) {
   priority_queue<VertexCost, vector<VertexCost>, decltype(&GreaterCost)>
       pq(&GreaterCost);
-  unordered_map<int, double> vertex_id_to_min;
   for (const Edge& edge : graph[start]) {
     VertexCost state({edge.to, start, 1, static_cast<double>(edge.distance)});
     pq.push(state);
-    int vertex_id = state.VertexID();
-    if (vertex_id_to_min.find(vertex_id) == vertex_id_to_min.end()) {
-      vertex_id_to_min[vertex_id] = state.cost;
-    } else if (state.cost < vertex_id_to_min[vertex_id]) {
-      vertex_id_to_min[vertex_id] = state.cost;
-    } else {
-      continue;
-    }
   }
-  unordered_set<int> visited;
+  vector<vector<vector<bool>>>
+      visited(n, vector<vector<bool>>(n, vector<bool>(MAX_SPEED + 1, false)));
   while (!pq.empty()) {
-    VertexCost state = pq.top();
-    pq.pop();
-    int vertex_id = state.VertexID();
-    if (visited.find(vertex_id) != visited.end()) continue;
-    visited.insert(vertex_id);
+    VertexCost state = pq.top(); pq.pop();
+    if (visited[state.city][state.prev][state.speed]) continue;
+    visited[state.city][state.prev][state.speed] = true;
 
     if (state.city == goal && state.speed == 1) {
       cout << state.cost << endl;
@@ -71,14 +55,6 @@ void Solve(int n, vector<vector<Edge>>& graph, int start, int goal) {
         if (next_speed > edge.max_speed) continue;
         double next_cost = state.cost + distance / next_speed;
         VertexCost next({next_city, curr_city, next_speed, next_cost});
-        int next_vertex_id = next.VertexID();
-        if (vertex_id_to_min.find(next_vertex_id) == vertex_id_to_min.end()) {
-          vertex_id_to_min[next_vertex_id] = next.cost;
-        } else if (next.cost < vertex_id_to_min[next_vertex_id]) {
-          vertex_id_to_min[next_vertex_id] = next.cost;
-        } else {
-          continue;
-        }
         pq.push(next);
       }
     }
